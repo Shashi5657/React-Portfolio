f78feabb89ef09ffb3e2565425c1bace2823840d
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-24 18:45:32 +0530

    in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+        <About />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/shashidharsangepu/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/Shashi5657"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/mr.villain29/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
