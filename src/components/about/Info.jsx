530edb8e23ca64e4c5b7781a496df7f6b816e5f6
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-29 21:26:38 +0530

    commits

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -9,6 +9,7 @@ const Info = () => {
         <span className="about__subtitle">1+ Years</span>
       </div>
 
+
       <div className="about__box">
         <i class="uil uil-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
